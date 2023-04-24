979866ca8cd7ac3fd598826f385d31486d284aa5
Dockerizer <712020:052cca29-aa81-4559-b029-0f8c3617dfc2@bots.bitbucket.org>
2023-04-24 22:28:25 +0200

    add widget

--- app/code/AlexYe/Catalog/view/frontend/web/js/demo-function.js
+++ app/code/AlexYe/Catalog/view/frontend/web/js/demo-function.js
@@ -3,9 +3,19 @@ define(
         'jquery',
     ],
     function ($) {
-        return function (){
-            alert('fdf123');
+        'use strict';
+        $.widget('alexYe.catalog_demoFunction',{
+            _create: function () {
+                var tag = $('<p></p>').html('Some message')
+                $(this.element).append(tag)
+            }
+        });
 
-        }
+        // return function (){
+        //     var tag = $('<p></p>').html('Some message')
+        //     $("#js-demo").append(tag)
+
+        // }
+        return $.alexYe.catalog_demoFunction;
     }
 );
