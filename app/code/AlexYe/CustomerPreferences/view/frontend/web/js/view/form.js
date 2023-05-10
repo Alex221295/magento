1218d75c91a42c9f22ec603497a09044bb6d7d7d
Dockerizer <712020:052cca29-aa81-4559-b029-0f8c3617dfc2@bots.bitbucket.org>
2023-05-10 22:22:32 +0200

    initialize modal

--- app/code/AlexYe/CustomerPreferences/view/frontend/web/js/view/form.js
+++ app/code/AlexYe/CustomerPreferences/view/frontend/web/js/view/form.js
@@ -14,68 +14,50 @@ define([
         },
         attributes: {},
 
-        savePreferences: function () {
-            console.log(this.attributes);
-        }
-    });
+        /**
+         * Watch customer data change and update input values
+         */
+        initObservable: function () {
+            var customerPreferences = customerData.get('customer-preferences')();
 
-    // Start rewriting form into the Knockout component
-    $.widget('alexYeCustomerPreferences.form', {
-        options: {
-            action: ''
+            this._super();
+
+            // @TODO: JS may break if new attributes are added
+            this.attributes.forEach(function (attributeData) {
+                attributeData.value = customerPreferences[attributeData['attribute_code']];
+            });
+
+            customerData.get('customer-preferences').subscribe(function (newCustomerPreferences) {
+                this.attributes.forEach(function (attributeData) {
+                    attributeData.value = newCustomerPreferences[attributeData['attribute_code']];
+                });
+            }.bind(this));
+
+            return this;
         },
 
         /**
-         * @private
+         * Init modal from the component HTML
          */
-        _create: function () {
-            this.modal = $(this.element).modal({
+        initModal: function () {
+            this.modal = $('#alex-ye-customer-preferences-form').modal({
                 buttons: []
             });
-
-            $(this.element).on('submit.alexYe_customerPreferences', $.proxy(this.savePreferences, this));
-
-            console.log(customerData.get('customer-preferences')());
-            customerData.get('customer-preferences').subscribe(function (value) {
-                console.log(value);
-            });
-        },
-
-        _destroy: function () {
-            this.modal.closeModal();
-            $(this.element).off('submit.alexYe_customerPreferences');
-            this.modal.destroy();
-        },
-
-        savePreferences: function () {
-            if (!this.validateForm()) {
-                return;
-            }
-
-            this.ajaxSubmit();
-        },
-
-        /**
-         * Validate request form
-         */
-        validateForm: function () {
-            return $(this.element).validation().valid();
         },
 
         /**
          * Submit request via AJAX. Add form key to the post data.
          */
-        ajaxSubmit: function () {
-            var formData = new FormData($(this.element).get(0));
-
-            formData.append('form_key', $.mage.cookies.get('form_key'));
-            formData.append('isAjax', 1);
+        savePreferences: function () {
+            var payload = {
+                attributes: this.attributes,
+                'form_key': $.mage.cookies.get('form_key'),
+                isAjax: 1
+            };
 
             $.ajax({
-                url: this.options.action,
-                data: formData,
-                processData: false,
-                contentType: false,
+                url: this.action,
+                data: payload,
                 type: 'post',
                 dataType: 'json',
                 context: this,
@@ -100,12 +82,17 @@ define([
                     $('body').trigger('processStop');
                     alert({
                         title: $.mage.__('Error'),
-                        content: $.mage.__('Your preferences can\'t be saved. Please, contact us if ypu see this message.')
+                        content: $.mage.__(
+                            'Your preferences can\'t be saved. Please, contact us if ypu see this message.'
+                        )
                     });
+                },
+
+                /** @inheritdoc */
+                complete: function () {
+                    this.modal.modal('closeModal');
                 }
             });
-        },
+        }
     });
-
-    return $.alexYeCustomerPreferences.form;
 });
